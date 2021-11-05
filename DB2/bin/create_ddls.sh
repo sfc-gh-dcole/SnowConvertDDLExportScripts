ae50e5f3721b4962f16466e86e77aa18949407cb
Orellabac <orellabac@gmail.com>
2021-11-05 15:54:56 +0000

    update export script and usage

--- DB2/bin/create_ddls.sh
+++ DB2/bin/create_ddls.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 echo "DB2 DDL Export script"
 echo "Getting list of databases"
-OUTPUTDIR="./output"
+OUTPUTDIR="../object_extracts"
 ### Get List of Database
 
 ## You can modify this variable to exclude some databases:
@@ -11,11 +12,12 @@ OUTPUTDIR="./output"
 ## You can use regular any valid regular expression as a pattern to exclude the databases to exclude
 DATABASES_TO_EXCLUDE="XXXXXXX"
 
-
+DDLS="$OUTPUTDIR/DDL"
+mkdir -p $DDLS
 DBS=$( db2 list db directory | grep Indirect -B 5 |grep "Database alias" |awk {'print $4'} |sort -u | uniq 2>/dev/null | grep -v -E $DATABASES_TO_EXCLUDE)
 for db in $DBS
 do
-    mkdir -p "$OUTPUT/$db"
+    mkdir -p "$DDLS/$db"
     echo "Processing Database $db"
-    db2look -d $db -e -l > "$OUTPUT/$db/DDL_All.sql"
+    db2look -d $db -e -l > "$DDLS/$db/DDL_All.sql"
 done
